c74ee123e03a336d3babd90006d32365604e4246
rotemuri <rotemuri@amazon.com>
2022-02-17 21:52:45 -0500

    fan-experience fix amplify cutome headers

--- source/mre-fan-experience-frontend/cdk/init-amplify.py
+++ source/mre-fan-experience-frontend/cdk/init-amplify.py
@@ -14,6 +14,7 @@ def main(region, profile=None):
     user_pool_id = cdk_outputs_json['mre-fan-experience-frontend-stack']['userPoolId']
     identity_pool_id = cdk_outputs_json['mre-fan-experience-frontend-stack']['identityPoolId']
     app_client_id = cdk_outputs_json['mre-fan-experience-frontend-stack']['appClientId']
+    web_url = cdk_outputs_json['mre-fan-experience-frontend-stack']['webAppURL']
 
     if profile:
         boto3.setup_default_session(profile_name=profile)
@@ -29,6 +30,22 @@ def main(region, profile=None):
         Name='/MRE/DataPlane/EndpointURL',
         WithDecryption=False
     )
+
+    bucket_parameter = ssm_client.get_parameter(
+        Name='/MRE/ControlPlane/MediaOutputBucket',
+        WithDecryption=False
+    )
+
+    control_plain_endpoint_domain = '/'.join(control_plain_endpoint['Parameter']['Value'].split('/')[0:-2])
+    data_plane_endpoint_domain = '/'.join(data_plane_endpoint['Parameter']['Value'].split('/')[0:-2])
+    bucket_name = bucket_parameter['Parameter']['Value']
+
+    media_output_distro = ssm_client.get_parameter(
+         Name='/MRE/ControlPlane/MediaOutputDistribution',
+         WithDecryption=False
+     )
+
+    cloudfront_url = f"https://{media_output_distro['Parameter']['Value']}"
 
     amplify_client = boto3.client('amplify', region_name=region)
 
@@ -48,16 +65,33 @@ def main(region, profile=None):
                 value: 'nosniff'
               - key: 'Content-Security-Policy'
                 value: >-
-                    default-src 'self' https://cognito-identity.{region}.amazonaws.com
-                    *.execute-api.{region}.amazonaws.com https://*.s3.amazonaws.com; style-src 'self'
-                    'unsafe-inline'; connect-src 'self'
-                    https://cognito-idp.{region}.amazonaws.com/
+                    default-src 'none'; style-src 'self' 'unsafe-inline';
+                    connect-src
+                    'self' https://cognito-idp.{region}.amazonaws.com
                     https://cognito-identity.{region}.amazonaws.com
-                    *.execute-api.{region}.amazonaws.com; script-src 'self'
-                    'unsafe-inline' https://cognito-idp.{region}.amazonaws.com/
+                    {control_plain_endpoint_domain}
+                    {data_plane_endpoint_domain}
+                    {cloudfront_url}/;
+                    script-src
+                    blob: 'self' 'unsafe-inline'
+                    https://{web_url}/
+                    https://cdn.jsdelivr.net/
+                    https://cognito-idp.{region}.amazonaws.com
                     https://cognito-identity.{region}.amazonaws.com
-                    *.execute-api.{region}.amazonaws.com; img-src 'self' https://*.s3.amazonaws.com;
-                    media-src 'self' https://*.s3.amazonaws.com;
+                    {control_plain_endpoint_domain}
+                    {data_plane_endpoint_domain};
+                    img-src 'self'
+                    https://{bucket_name}.s3.amazonaws.com;
+                    media-src 'self'
+                    blob:
+                    https://{web_url}/
+                    {cloudfront_url}/
+                    https://cdn.jsdelivr.net/
+                    https://{bucket_name}.s3.amazonaws.com;
+                    object-src 'none';frame-ancestors 'none';
+                    font-src 'self'
+                    https://{web_url}/;
+                    manifest-src 'self'
             ''',
         environmentVariables={
             "REACT_APP_BASE_API": control_plain_endpoint['Parameter']["Value"],
@@ -66,7 +100,7 @@ def main(region, profile=None):
             "REACT_APP_USER_POOL_ID": user_pool_id,
             "REACT_APP_APP_CLIENT_ID": app_client_id,
             "REACT_APP_IDENTITY_POOL_ID": identity_pool_id,
-            "REACT_APP_CLOUDFRONT_PREFIX": "replace_with_url"
+            "REACT_APP_CLOUDFRONT_PREFIX": cloudfront_url
         },
         customRules=[
             {
